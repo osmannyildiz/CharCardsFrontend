8120ef750fe02d0180f3eb7f4acf9ad69a868733
Osman Nuri Yıldız <iamosmannyildiz@gmail.com>
2022-10-27 12:57:28 +0300

    Add load more on scroll feature

--- src/components/characters/CharacterCardGrid.tsx
+++ src/components/characters/CharacterCardGrid.tsx
@@ -1,5 +1,9 @@
 import CharacterCard from "@/components/characters/CharacterCard";
+import { fetchCharactersNextPage } from "@/data/apiSlice";
+import { StoreDispatch } from "@/data/store";
 import type Character from "@/models/character";
+import { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 import "./CharacterCardGrid.css";
 
 interface Props {
@@ -7,8 +11,32 @@ interface Props {
 }
 
 export default function CharacterCardGrid({ characters }: Props) {
+	const gridRef = useRef<HTMLDivElement | null>(null);
+	const dispatch = useDispatch<StoreDispatch>();
+
+	const loadMore = async () => {
+		await dispatch(fetchCharactersNextPage());
+	};
+
+	// https://stackoverflow.com/a/45586395/19206552
+	const trackScroll = () => {
+		const el = gridRef.current;
+		if (el) {
+			if (el.getBoundingClientRect().bottom <= window.innerHeight) {
+				loadMore();
+			}
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener("scroll", trackScroll);
+		return () => {
+			document.removeEventListener("scroll", trackScroll);
+		};
+	}, []);
+
 	return (
-		<div className="character-card-grid">
+		<div className="character-card-grid" ref={gridRef}>
 			{characters.map((character) => (
 				<CharacterCard
 					key={character.id}
